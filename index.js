125129225224929b77eabc0ac6589fb47e3f891f
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2022-06-01 20:35:57 +0300

    Мод 10.1 http-запросы
    1. Добавила стили.
    2. Перенесла хранение данных на сервер.
    3. Ф-ции для изменения, добавления и удаления данных на сервере.

--- index.js
+++ index.js
@@ -2,30 +2,25 @@ const refs = {
     body: document.querySelector('body'),
     formEl: document.querySelector('#todo-form'),
     todoList: document.querySelector('#todo-list'),
-    formBtn:document.querySelector(".formBtn")
+    formBtn: document.querySelector(".formBtn"),
+    loader: document.querySelector('#loader'),
 
 }
 
+const fetchTodos = () => { }
+const createTodo = () => {}
+const deleteTodo = () => {}
+const updateTodo = () => {}
 
 const URL = `https://6296541a810c00c1cb73bacd.mockapi.io/todos`;
 let items = [];
 
 const loadData = () => {
-    try {
-        if (localStorage.getItem("todos")) {
-            items = JSON.parse(localStorage.getItem("todos"));
-        }
-    }catch (error){
-        console.log(error.message)
-        items = [];
-    }
-    // fetch(URL).then(resp => resp.json()).then(data => items = data).catch(error => console.log(error))
+    return fetch(URL)
+        .then(resp => resp.json())
+        .then(data => items = data)
+        .catch(error => console.log(error));
 }
-
-const saveData = () => {
-    localStorage.setItem("todos", JSON.stringify(items));
-}
-
 
 const createItems = ({ id, text, isDone }) =>{
    return `<li class="todo-item" data-id="${id}">
@@ -43,6 +38,98 @@ const renderList = (items) => {
     refs.todoList.innerHTML = "";
   refs.todoList.insertAdjacentHTML("beforeend", list); 
 }
+
+const showLoader = () => {
+    refs.loader.classList.add("show");
+
+}
+
+const hideLoader = () => {
+        refs.loader.classList.remove("show");
+
+}
+const createItem = (item) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body:JSON.stringify(item),
+}
+    return fetch(URL, options).then(resp => resp.json).catch(error => console.log(error));
+}
+
+const toggleItem = (id) => {
+
+ 
+}
+
+const deleteItem = (id) => {
+    return fetch(`${URL}/${id}`, {
+        method: "DELETE",
+    }).catch(error => console.log(error))
+}
+
+const onFormSubmit = (evt) => {
+    evt.preventDefault();
+    const inputValue = evt.target.elements.text.value;
+    const newItem = { text: inputValue, isDone: false };
+showLoader()
+    createItem(newItem).then(() => {
+    items.push(newItem);
+    }).then(() => {
+    renderList(items);
+    }).then(() => {
+    refs.formEl.reset();
+    }).finally(() => {
+        hideLoader()
+    })
+    
+}
+
+refs.formEl.addEventListener("submit", onFormSubmit)
+
+
+
+
+const handleListClick = (evt) => {
+    if (evt.target === evt.currentTarget) return;
+    const parent = evt.target.closest('li');
+    const { id } = parent.dataset;
+    if (evt.target.nodeName === "INPUT") {
+        toggleItem(id)
+            .then(() => {
+             items = items.map(item => item.id === id ? { ...item, isDone: !item.isDone } : item);
+            })
+            .then(() => renderList(items))
+            .catch(error => console.log(error))
+        
+}
+if (evt.target.nodeName === "BUTTON") {
+    deleteItem(id)
+        .then(() => items = items.filter(item => item.id !== id))
+        .then(() => renderList(items))
+        .catch(error => console.log(error))
+    }
+}
+
+refs.todoList.addEventListener("click", handleListClick);
+ 
+const loadAndRender = () => {
+    showLoader();
+    loadData().then(() => {
+        renderList(items);
+    }).catch(error => console.log(error))
+    .finally(() => {
+        hideLoader()
+    })
+       
+
+}
+
+loadAndRender()
+
+
 
    
 // const createForm = () => {
@@ -63,48 +150,18 @@ const renderList = (items) => {
 // }
 // createForm();
 
-const onFormSubmit = (evt) => {
-    evt.preventDefault();
-    const inputValue = evt.target.elements.text.value;
-    const newItem = { id: Date.now().toString(), text: inputValue, isDone: false };
-    items.push(newItem);
-    saveData();
-    renderList(items);
-    refs.formEl.reset();
-}
+//================ Сохранение данных в веб-хранилищи ===========//
+// const loadData = () => {
+    // try {
+    //     if (localStorage.getItem("todos")) {
+    //         items = JSON.parse(localStorage.getItem("todos"));
+    //     }
+    // }catch (error){
+    //     console.log(error.message)
+    //     items = [];
+    // }}
 
-refs.formEl.addEventListener("submit", onFormSubmit)
-
-
-const toggleItem = (id) => {
-    items = items.map(item => item.id === id? {...item, isDone: !item.isDone} : item)
-
-}
-
-const deleteItem = (id) => {
-    items = items.filter(item => item.id !== id);
-}
-
-const handleListClick = (evt) => {
-    if (evt.target === evt.currentTarget) return;
-    const parent = evt.target.closest('li');
-    const { id } = parent.dataset;
-    if (evt.target.nodeName === "INPUT") {
-        toggleItem(id);
-        saveData();
-        renderList(items);
-}
-if (evt.target.nodeName === "BUTTON") {
-    deleteItem(id);
-    saveData();
-        renderList(items);
-
-    }
-}
-
-refs.todoList.addEventListener("click", handleListClick);
- 
-loadData();
-renderList(items);
-
+    // const saveData = () => {
+    // localStorage.setItem("todos", JSON.stringify(items));
+// }
 
